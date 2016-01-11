86b5eaade2f8dc4e5a6b30dd183c822032a898bb
Em Dash Buck <em.dash.buck@gmail.com>
2016-01-11 15:31:30 -0800

    Project todo update
    Got GitHub set up

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Em Dash Projects</title>
+</head>
+
+<body>
+<h2>To Do List:</h2>
+<p>
+<ul>
+<li><strike>Get GitHub linked to projects folder in DropBox (technically redundant but provides two different services)</strike></li>
+<li>Set alarm on phone for daily commit (8:00 pm?)</li>
+<li><a href="codecademy.com">Finish brushing up on JavaScript</a></li>
+<li><strike>Create HTML skeleton</strike></li>
+<li>Create HTML form base</li>
+<li>Write PHP and MySQL bits</li>
+<li>Add JavaScript bits</li>
+<li>Write project intro blog post, link here</li>
+<li>Tweet about blog post</li>
+<li>Schedule weekly blog post time</li>
+</ul>
+</p>
+
+<h2>Daily Commit Checklist</h2>
+<ol>
+<li>GitHub commit</li>
+<li>Write notes re: what happened today in project land</li>
+<li>Set goal for tomorrow</li>
+</ol>
+
+<h2>Minimum Viable Product Features:</h2>
+<p>
+<ul>
+<li>List item display</li>
+<li>List item input</li>
+<li>List item delete</li>
+<li>List save</li>
+</ul>
+<ul>
+<li>List checkbox</li>
+<li>List dot</li>
+</ul>
+</p>
+</body>
+
+</html>
