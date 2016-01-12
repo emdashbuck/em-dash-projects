7ca759c0ca20b5df208c146d023416a12fdd074e
Em Dash Buck <em.dash.buck@gmail.com>
2016-01-11 22:36:48 -0800

    Setup. HTML and CSS rough draft. save.php will be Save function handler. MVP ToDo Day 1.

--- index.php
+++ index.php
@@ -2,44 +2,61 @@
 <html>
 <head>
 <title>Em Dash Projects</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
 <h2>To Do List:</h2>
+<form action="save.php" method="post">
+  Task Item:<input type="text" name="task">
+  <input type="submit" value="Save">
+</form>
 <p>
-<ul>
-<li><strike>Get GitHub linked to projects folder in DropBox (technically redundant but provides two different services)</strike></li>
-<li>Set alarm on phone for daily commit (8:00 pm?)</li>
-<li><a href="codecademy.com">Finish brushing up on JavaScript</a></li>
-<li><strike>Create HTML skeleton</strike></li>
-<li>Create HTML form base</li>
-<li>Write PHP and MySQL bits</li>
-<li>Add JavaScript bits</li>
-<li>Write project intro blog post, link here</li>
-<li>Tweet about blog post</li>
-<li>Schedule weekly blog post time</li>
-</ul>
+	<ul>
+		<li class="x">Get GitHub linked to projects folder in DropBox (technically redundant but provides two different services)</li>
+		<li class="x">Set alarm on phone for daily project checkin (8:00 pm?)</li>
+		<li><a href="http://www.codecademy.com">Finish brushing up on JavaScript</a></li>
+		<li class="x">Create HTML page</li>
+		<li class="x">Create HTML/CSS form rough draft</li>
+		<li>Create MySQL database</li>
+		<li>Create MySQL database table
+			<ul>
+				<li>How many columns? Talk to Fox re db design</li>
+			</ul></li>
+		<li>MySQL database call</li>
+		<li>PHP display database entry</li>
+		<li>PHP form submit handler</li>
+		<li>JS cross out, delete, add</li>
+		<li>Clean up CSS</li>
+		<li>Write project intro blog post, link here</li>
+		<li>Tweet about blog post</li>
+		<li>Schedule weekly blog post time (Sunday?)</li>
+		<li>Create project milestones</li>
+		<li>Schedule milestones</li>
+	</ul>
 </p>
 
-<h2>Daily Commit Checklist</h2>
+<h2>Daily and Weekly Project Checkins</h2>
+<p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish</em></p>
 <ol>
-<li>GitHub commit</li>
-<li>Write notes re: what happened today in project land</li>
-<li>Set goal for tomorrow</li>
+	<li>Week 1<ol>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-day-1/">Day 1</a></li>
+		</ol>
+	</li>
 </ol>
 
 <h2>Minimum Viable Product Features:</h2>
 <p>
-<ul>
-<li>List item display</li>
-<li>List item input</li>
-<li>List item delete</li>
-<li>List save</li>
-</ul>
-<ul>
-<li>List checkbox</li>
-<li>List dot</li>
-</ul>
+	<ul>
+		<li>List item display</li>
+		<li>List item input</li>
+		<li>List item delete</li>
+		<li>List save</li>
+	</ul>
+	<ul>
+		<li>List checkbox</li>
+		<li>List dot</li>
+	</ul>
 </p>
 </body>
 
---
+++ save.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+* Placeholder for PHP handler for save. Will eventually deliver the current list to the database.
+**/
+
+echo "Saaaaaave?";
+
+?>
