0a700e8cb54a29c1ee548f20082a4d355884ce58
Em Dash Buck <em.dash.buck@gmail.com>
2016-01-13 03:21:33 -0800

    MIlestones established. CSS "dot" created.
    See MVP ToDo Daily Report 2 for more details.

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
 	<ul>
 		<li class="x">Get GitHub linked to projects folder in DropBox (technically redundant but provides two different services)</li>
 		<li class="x">Set alarm on phone for daily project checkin (8:00 pm?)</li>
-		<li><a href="http://www.codecademy.com">Finish brushing up on JavaScript</a></li>
+		<li><a href="http://www.codecademy.com">Finish brushing up on JS</a></li>
 		<li class="x">Create HTML page</li>
 		<li class="x">Create HTML/CSS form rough draft</li>
 		<li>Create MySQL database</li>
 		<li>Create MySQL database table
 			<ul>
-				<li>How many columns? Talk to Fox re db design</li>
+				<li class="o">How many columns? Talk to Fox re db design</li>
 			</ul></li>
 		<li>MySQL database call</li>
 		<li>PHP display database entry</li>
@@ -30,9 +30,41 @@
 		<li>Clean up CSS</li>
 		<li>Write project intro blog post, link here</li>
 		<li>Tweet about blog post</li>
-		<li>Schedule weekly blog post time (Sunday?)</li>
-		<li>Create project milestones</li>
-		<li>Schedule milestones</li>
+		<li class="x">Schedule weekly blog post time (Sunday?)</li>
+		<li class="x">Create project milestones</li>
+		<li class="x">Schedule milestones</li>
+		<li class="o">Clean up task list to align with Milestones.</li>
+		<li>Create Google Calendar for milestones?</li>
+		<li>Update Em Dash Blog to be ready for public viewing<ul>
+			<li>Blog roll design</li>
+			<li>Background image - minimize loading time</li>
+			<li>single-post.php - update design?</li>
+			<li>Sidebar and sidebar content</li>
+			<li>Update intro page content</li>
+			</ul>			
+		<li>Content and advertising plan<ul>
+			<li>Write support content to introduce project and/or blog</li>
+			<li>Choose social media foci<ol>
+				<li>Twitter</li>
+				<li>Facebook?</li>
+				<li>LinkedIn?</li>
+				<li>Tumblr?</li>
+				<li>Pinterest? (note: will require post art. Might be a good idea down the line)</li>
+				<li>Reddit?</li>
+				<li>Mark Forster forum (after launch, possibly after addition of FV/P specific features)</li>
+				</ol>
+			</li>
+			<li>Plan announcements for chosen social mediums</li>
+			<li>Choose autoposter if necessary</li>
+			</ul>
+		</li>
+		<li class="o">Weekly Review Setup<ul>
+			<li>Add to calendar</li>
+			<li>Set alarm</li>
+			<li>Write routine</li>
+			</ul>
+		</li>
+		
 	</ul>
 </p>
 
@@ -40,24 +72,53 @@
 <p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish</em></p>
 <ol>
 	<li>Week 1<ol>
-		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-day-1/">Day 1</a></li>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-1/">Day 1</a></li>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-2/">Day 2</a></li>
 		</ol>
 	</li>
 </ol>
 
-<h2>Minimum Viable Product Features:</h2>
+<h2>Project Milestones</h2>
+<ol>
+	<li>Week 1: HTML/CSS skeleton<ul>
+			<li class="x">Display</li>
+			<li class="x">Checkbox</li>
+			<li class="x">Dot</li>
+			<li>Save/Load form</li>
+			</ul>
+	</li>
+	<li>Week 2: Em Dash Blog prep</li>
+	<li>Week 3: MySQL Database Setup</li>
+	<li>Week 4:PHP Save Handler</li>
+	<li>Week 5:PHP Load Handler</li>
+	<li>Week 6: JS Visuals (Toggle on click dot->check->box)</li>
+	<li>Week 7: JS Delete Button<ul>
+		<li>On click: Popup alert "Are you shure you want to delete?"</li>
+		<li>On okay, remove line item from text area</li>
+		</ul>
+	</li>
+	<li>Week 8: JS Click to edit line items</li>
+	<li>Week 9: Testing</li>
+	<li>Week 10: UI Design & Dev</li>
+	<li>Week 11: Final Tweaks</li>
+	<li>Week 12: Launch</li>
+</ol>
+	
+<h2>Minimum Viable ToDo List Features:</h2>
 <p>
 	<ul>
-		<li>List item display</li>
-		<li>List item input</li>
-		<li>List item delete</li>
-		<li>List save</li>
+		<li>List item display (HTML/CSS/PHP,JS?)</li>
+		<li>List item input (JS)</li>
+		<li>List item add (JS)</li>
+		<li>List item delete (JS)</li>
+		<li>List save (PHP/MySQL)</li>
+		<li>List load (PHP/MySQL)</li>
 	</ul>
 	<ul>
-		<li>List checkbox</li>
-		<li>List dot</li>
+		<li>Toggle list checkbox (CSS/JS)</li>
+		<li>Toggle list dot (CSS/JS)</li>
+		<li>List item edit (JS)</li>
 	</ul>
 </p>
 </body>
-
 </html>
