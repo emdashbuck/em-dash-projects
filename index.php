e7d6d0b1d40d9b4a66ed8c15d87ae4bb95174cbb
Em Dash Buck <em.dash.buck@gmail.com>
2016-01-14 00:23:29 -0800

    Reorganize todo list, primary/secondary divs
    See MVP ToDo Day 3 for details

--- index.php
+++ index.php
@@ -6,97 +6,115 @@
 </head>
 
 <body>
+<div id="primary">
 <h2>To Do List:</h2>
 <form action="save.php" method="post">
   Task Item:<input type="text" name="task">
   <input type="submit" value="Save">
 </form>
 <p>
-	<ul>
+<ol>
+	<li>Setup, etc.<ul>
 		<li class="x">Get GitHub linked to projects folder in DropBox (technically redundant but provides two different services)</li>
 		<li class="x">Set alarm on phone for daily project checkin (8:00 pm?)</li>
-		<li><a href="http://www.codecademy.com">Finish brushing up on JS</a></li>
-		<li class="x">Create HTML page</li>
-		<li class="x">Create HTML/CSS form rough draft</li>
-		<li>Create MySQL database</li>
-		<li>Create MySQL database table
-			<ul>
-				<li class="o">How many columns? Talk to Fox re db design</li>
-			</ul></li>
-		<li>MySQL database call</li>
-		<li>PHP display database entry</li>
-		<li>PHP form submit handler</li>
-		<li>JS cross out, delete, add</li>
-		<li>Clean up CSS</li>
-		<li>Write project intro blog post, link here</li>
-		<li>Tweet about blog post</li>
 		<li class="x">Schedule weekly blog post time (Sunday?)</li>
 		<li class="x">Create project milestones</li>
 		<li class="x">Schedule milestones</li>
-		<li class="o">Clean up task list to align with Milestones.</li>
-		<li>Create Google Calendar for milestones?</li>
-		<li>Update Em Dash Blog to be ready for public viewing<ul>
-			<li>Blog roll design</li>
+		<li class="x">Clean up task list to align with milestones</li>
+		<li class="maybe">Create Google Calendar for milestones?</li>
+		<li class="x">Weekly Review Setup</li>
+		<li class="o">Design decision: Copypasta test or save/load function?</li>
+		</ul></li>
+	<li>Week 1: HTML/CSS skeleton<ul>
+		<li class="x">Create HTML/CSS rough draft</li>
+		<li class="x">Display/Content</li>
+		<li class="x">Checkbox</li>
+		<li class="x">Dot</li>
+		<li>Save/Load form</li>
+		</ul></li>
+	<li>Week 2: Em Dash Blog prep<ul>
+		<li>Design<ul>
+			<li>Overall design sketch<ul>
+				<li>Look at Christine portfolio for inspiration</li>
+				</ul>
+			</li>
+			<li>archive.php</li>
+			<li>single-post.php</li>
+			<li>home.php</li>
 			<li>Background image - minimize loading time</li>
-			<li>single-post.php - update design?</li>
-			<li>Sidebar and sidebar content</li>
-			<li>Update intro page content</li>
-			</ul>			
-		<li>Content and advertising plan<ul>
-			<li>Write support content to introduce project and/or blog</li>
-			<li>Choose social media foci<ol>
+			<li>Get feedback from Christine</li>
+			</ul>
+		</li>
+		<li>Content<ul>
+			<li>Intro to project</li>
+			<li>Intro to Dash</li>
+			<li style="maybe">Intro to the blog?</li>
+			<li>Sidebar content</li>
+			<li>Homepage/intro page update</li>
+			<li>Social media hook/s</li>
+			</ul>
+		</li>
+		<li>Social Media<ol>
+			<li>Choose foci<ol>
 				<li>Twitter</li>
-				<li>Facebook?</li>
-				<li>LinkedIn?</li>
-				<li>Tumblr?</li>
-				<li>Pinterest? (note: will require post art. Might be a good idea down the line)</li>
-				<li>Reddit?</li>
+				<li style="maybe">Facebook?</li>
+				<li style="maybe">LinkedIn?</li>
+				<li style="maybe">Tumblr?</li>
+				<li style="maybe">Pinterest? (note: will require post art. Might be a good idea down the line)</li>
+				<li style="maybe">Reddit? (would need to find the right subreddit)</li>
 				<li>Mark Forster forum (after launch, possibly after addition of FV/P specific features)</li>
 				</ol>
 			</li>
-			<li>Plan announcements for chosen social mediums</li>
-			<li>Choose autoposter if necessary</li>
-			</ul>
+			<li style="maybe">Research autoposters?</li>
+			</ol>
 		</li>
-		<li class="o">Weekly Review Setup<ul>
-			<li>Add to calendar</li>
-			<li>Set alarm</li>
-			<li>Write routine</li>
-			</ul>
-		</li>
-		
-	</ul>
-</p>
-
-<h2>Daily and Weekly Project Checkins</h2>
-<p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish</em></p>
-<ol>
-	<li>Week 1<ol>
-		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-1/">Day 1</a></li>
-		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-2/">Day 2</a></li>
-		</ol>
-	</li>
-</ol>
-
-<h2>Project Milestones</h2>
-<ol>
-	<li>Week 1: HTML/CSS skeleton<ul>
-			<li class="x">Display</li>
-			<li class="x">Checkbox</li>
-			<li class="x">Dot</li>
-			<li>Save/Load form</li>
-			</ul>
-	</li>
-	<li>Week 2: Em Dash Blog prep</li>
-	<li>Week 3: MySQL Database Setup</li>
-	<li>Week 4:PHP Save Handler</li>
-	<li>Week 5:PHP Load Handler</li>
-	<li>Week 6: JS Visuals (Toggle on click dot->check->box)</li>
+		</ul></li>
+	<li>Week 3: MySQL Database Setup<ul>
+		<li class="x">How many columns? Talk to Fox re db design</li>
+		<li>Create MySQL database</li>
+		<li>Create MySQL database table</li>
+		</ul></li>
+	<li>Week 4:PHP Save Handler<ul>
+		<li>MySQL database call</li>
+		<li>PHP display database entry</li>
+		<li>PHP form submit handler</li>
+		</ul></li>
+	<li>Week 5:PHP Load Handler<ul>
+		<li>PHP/MySQL user login</li>
+		<li>MySQL database call: lists</li>
+		<li>PHP display database entry</li>
+		<li>PHP form submit handler</li>
+		</ul></li>
+	<li>Week 6: JS Visuals (Toggle on click dot->check->box)<ul>
+		<li><a href="http://www.codecademy.com">Finish brushing up on JS</a></li>
+		</ul></li>
 	<li>Week 7: JS Delete Button<ul>
 		<li>On click: Popup alert "Are you shure you want to delete?"</li>
 		<li>On okay, remove line item from text area</li>
 		</ul>
 	</li>
+	<li>Week 8: JS Click to edit line items<ul></ul></li>
+	<li>Week 9: Testing<ul></ul></li>
+	<li>Week 10: UI Design & Dev<ul></ul></li>
+	<li>Week 11: Final Tweaks<ul>
+		<li>Clean up CSS, add comments</li>
+		<li>Clean up PHP, add comments</li>
+		<li>Clean up HTML/JS, add comments</li>
+		</ul></li>
+	<li>Week 12: Launch<ul></ul></li>
+</ol>
+</p>
+</div>
+<div id="secondary">
+<h2>Project Milestones</h2>
+<ol>
+	<li>Week 1: HTML/CSS skeleton</li>
+	<li>Week 2: Em Dash Blog prep</li>
+	<li>Week 3: MySQL Database Setup</li>
+	<li>Week 4:PHP Save Handler</li>
+	<li>Week 5:PHP Load Handler</li>
+	<li>Week 6: JS Visuals (Toggle on click dot->check->box)</li>
+	<li>Week 7: JS Delete Button</li>
 	<li>Week 8: JS Click to edit line items</li>
 	<li>Week 9: Testing</li>
 	<li>Week 10: UI Design & Dev</li>
@@ -120,5 +138,47 @@
 		<li>List item edit (JS)</li>
 	</ul>
 </p>
+<h2>Daily and Weekly Project Checkins</h2>
+<p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish</em></p>
+<ol>
+	<li>Week 1<ol>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-1/">Day 1</a></li>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-2/">Day 2</a></li>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-3/">Day 3</a></li>
+		</ol>
+	</li>
+</ol>
+
+<h2>Weekly Review</h2>
+<ol class="num">
+	<li>Pack bag (Make sure I have everything that keeps me focused and comfortable with me)</li>
+	<li>Shower &amp; dress (important for mindset)</li>
+	<li>Travel to Working Location  (Not home, so easier to do work instead of relax/play)<ul>
+		<li>Coffee Shop</li>
+		<li>Desk (Basically pretend to be at coffee shop)<ol>
+			<li>Put everything other than tech hookups elsewhere</li>
+			<li>Put bag next to chair on the floor</li>
+			<li>Make fancy drink</li>
+			</ol>
+		</li>
+		</ul>
+	</li>
+	<li>Write general idea about how week went, how the weekend went, etc. (Journalling is a standard warmup for me, gets me in the right mindset and also allows me to write out anything that's on my mind and could get in the way.)</li>
+	<li>Read Daily Reports and take notes. Mark questions and conclusions strongly so they'll get answered and included. (Review, and see if my general idea of how the week went matches my records of how the week went. Questions and conclusions that occur during this review will form the backbone of the Weekly Report.)</li>
+	<li>Rough out Weekly Report. May include the following topics (will probably need to be adjusted as I figure out how to write these):<ul>
+		<li>Intro to week's milestone, how the milestone relates to the goal of the project</li>
+		<li>Whether or not the milestone is complete and why</li>
+		<li>Breakdown what happened by task if necessary</li>
+		<li>Any meta conclusions about the system or suggested adjustments</li>
+		<li>Adjustments to the timeline and next week's milestone</li>
+		<li>Conclusion: What needs work, what went well, what's next.</li>
+		</ul>
+	</li>
+	<li>Final edit of last week's Weekly Report. (Need time to pass and come back with fresh eyes.)</li>
+	<li>Final read-through and publish. (Separate from final edit in order to catch any new errors etc that may have been introduced during edit.)</li>
+	<li>Announce on social media. (If I don't tell people I wrote something, no one will see it.)</li>
+	<li>Rewarding activity/item, e.g. dance party, danish, etc. (Important for habit reinforcement! Do not skip!)</li>
+</ol>
+</div>
 </body>
 </html>
