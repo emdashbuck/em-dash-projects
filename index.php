adda457561353133ad85e64d26b076ee3990f094
Em Dash Buck <em.dash.buck@gmail.com>
2016-01-15 01:09:56 -0800

    Planning: nix save/load, add HTML5
    MVP ToDo 4

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
 </head>
 
 <body>
+<div id="app">
+</div>
 <div id="primary">
 <h2>To Do List:</h2>
-<form action="save.php" method="post">
-  Task Item:<input type="text" name="task">
-  <input type="submit" value="Save">
-</form>
 <p>
 <ol>
 	<li>Setup, etc.<ul>
@@ -23,19 +21,21 @@
 		<li class="x">Clean up task list to align with milestones</li>
 		<li class="maybe">Create Google Calendar for milestones?</li>
 		<li class="x">Weekly Review Setup</li>
-		<li class="o">Design decision: Copypasta test or save/load function?</li>
+		<li class="x">Design decision: Copypasta test or save/load function?</li>
+		<li class="x">Replace JS tasks with HTML5/JS tasks</li>
+		<li class="x">Replace Save/Load tasks with editable text area tasks</li>
 		</ul></li>
 	<li>Week 1: HTML/CSS skeleton<ul>
 		<li class="x">Create HTML/CSS rough draft</li>
 		<li class="x">Display/Content</li>
 		<li class="x">Checkbox</li>
 		<li class="x">Dot</li>
-		<li>Save/Load form</li>
 		</ul></li>
 	<li>Week 2: Em Dash Blog prep<ul>
 		<li>Design<ul>
 			<li>Overall design sketch<ul>
-				<li>Look at Christine portfolio for inspiration</li>
+				<li class="o">Look at Christine portfolio for inspiration</li>
+				<li class="o">Experiment with possible design tweaks on paper, paint.net or Chrome Inspector</li>
 				</ul>
 			</li>
 			<li>archive.php</li>
@@ -43,65 +43,74 @@
 			<li>home.php</li>
 			<li>Background image - minimize loading time</li>
 			<li>Get feedback from Christine</li>
+			<li>Set up Google Analytics</li>
 			</ul>
 		</li>
 		<li>Content<ul>
 			<li>Intro to project</li>
 			<li>Intro to Dash</li>
-			<li style="maybe">Intro to the blog?</li>
+			<li class="maybe">Intro to the blog?</li>
 			<li>Sidebar content</li>
 			<li>Homepage/intro page update</li>
 			<li>Social media hook/s</li>
+			<li>Pro Tumblr handle, title, and description</li>
+			<li class="o">Pro Tumblr content (~100 words each. If explodes into >500 words, queue for blog instead and link to on Tumblr/Twitter with quote.)</li>
 			</ul>
 		</li>
-		<li>Social Media<ol>
-			<li>Choose foci<ol>
-				<li>Twitter</li>
-				<li style="maybe">Facebook?</li>
-				<li style="maybe">LinkedIn?</li>
-				<li style="maybe">Tumblr?</li>
-				<li style="maybe">Pinterest? (note: will require post art. Might be a good idea down the line)</li>
-				<li style="maybe">Reddit? (would need to find the right subreddit)</li>
-				<li>Mark Forster forum (after launch, possibly after addition of FV/P specific features)</li>
-				</ol>
-			</li>
-			<li style="maybe">Research autoposters?</li>
+		<li>Social Media<ul>
+			<em>Daily misc content, weekly blog updates, and call for testers</em>
+			<li>Facebook - Casual mention on personal account when post goes up</li>
+			<li>Tumblr<ul>
+				<li>Create "pro" tumblr: focus is on creating and my special interests (organization, systems, UI, process improvement, etc)<ul>
+					<li>Theme</li>
+					<li>Find tumblrs to follow: Design (graphic, video games, UI), writing (esp technical), coding, crafting, asthetic</li>
+					<li>Fill up the queue: Tag, use content from Twitter/Facebook if nesc, minimum capslock, no og swearing (reblogging swears ok if content is worth it). Balance between Tumblr linguistics and pro speak as best you can.</li>
+					<li>Set queue to drop 1-4 posts a day, one of which is og content/commentary</li>
+					<li>Add link to Pro tumblr in description of main tumblr</li>
+					<li>Advertise Pro tumblr on main tumblr 1/day for a few weeks</li>
+				</ul></li>
+			<li class="maybe">Research autoposters?</li>
 			</ol>
 		</li>
 		</ul></li>
-	<li>Week 3: MySQL Database Setup<ul>
-		<li class="x">How many columns? Talk to Fox re db design</li>
-		<li>Create MySQL database</li>
-		<li>Create MySQL database table</li>
+	<li>Week 3: HTML5 Editable Area<ul>
+		<li class="o"><a href="http://www.w3schools.com/html/html5_intro.asp">Brush up on HTML5</a></li>
 		</ul></li>
-	<li>Week 4:PHP Save Handler<ul>
-		<li>MySQL database call</li>
-		<li>PHP display database entry</li>
-		<li>PHP form submit handler</li>
+	<li>Week 4: HTML5/JS Visuals (Toggle on click dot->check->box)<ul>
+		<li class="maybe"><a href="http://www.codecademy.com">Finish brushing up on JS</a></li>
 		</ul></li>
-	<li>Week 5:PHP Load Handler<ul>
-		<li>PHP/MySQL user login</li>
-		<li>MySQL database call: lists</li>
-		<li>PHP display database entry</li>
-		<li>PHP form submit handler</li>
-		</ul></li>
-	<li>Week 6: JS Visuals (Toggle on click dot->check->box)<ul>
-		<li><a href="http://www.codecademy.com">Finish brushing up on JS</a></li>
-		</ul></li>
-	<li>Week 7: JS Delete Button<ul>
-		<li>On click: Popup alert "Are you shure you want to delete?"</li>
+	<li>Week 5: HTML5/JS Delete Button<ul>
+		<li>On click: Popup alert "Are you sure you want to delete?"</li>
 		<li>On okay, remove line item from text area</li>
 		</ul>
 	</li>
-	<li>Week 8: JS Click to edit line items<ul></ul></li>
-	<li>Week 9: Testing<ul></ul></li>
-	<li>Week 10: UI Design & Dev<ul></ul></li>
+	<li>Week 6: Testing<ul></ul></li>
+	<li>Week 7: UI Design & Dev<ul></ul></li>
+	<li>Week 8: <ul></ul></li>
+	<li>Week 9: <ul></ul></li>
+	<li>Week 10: <ul></ul></li>
 	<li>Week 11: Final Tweaks<ul>
 		<li>Clean up CSS, add comments</li>
 		<li>Clean up PHP, add comments</li>
 		<li>Clean up HTML/JS, add comments</li>
 		</ul></li>
 	<li>Week 12: Launch<ul></ul></li>
+</ol>
+
+<h2>Possible Future Features/Activities</h2>
+<ol>
+	<li>Add Web Storage (HTML5 cookie equiv) for local save/load option</li>
+	<li>Cntrl-Z</li>
+	<li>Multiple lists</li>
+	<li>Import/export: .csv, .txt, Markdown, .html</li>
+	<li>Active/Done list (disappear finished items)</li>
+	<li>Stats (when you checked off items, how long your streaks are on average, the oldest item, etc) </li>
+	<li>Social Media Later<ol>
+		<li>Mark Forster forum - after launch. Call for testers when adding additional algorithm-specfic features.</li>
+		<li>LinkedIn - include in Projects area once complete</li>
+		<li>Pinterest - will require post art. Use if I write an instructional post, e.g., "How to do personal project management"</li>
+		<li>Reddit - Similar to Pinterest. Will need to be posted in the right place. Requires research.</li>
+		</ol></li>
 </ol>
 </p>
 </div>
@@ -110,14 +119,14 @@
 <ol>
 	<li>Week 1: HTML/CSS skeleton</li>
 	<li>Week 2: Em Dash Blog prep</li>
-	<li>Week 3: MySQL Database Setup</li>
-	<li>Week 4:PHP Save Handler</li>
-	<li>Week 5:PHP Load Handler</li>
-	<li>Week 6: JS Visuals (Toggle on click dot->check->box)</li>
-	<li>Week 7: JS Delete Button</li>
-	<li>Week 8: JS Click to edit line items</li>
-	<li>Week 9: Testing</li>
-	<li>Week 10: UI Design & Dev</li>
+	<li>Week 3: HTML5 editable text area</li>
+	<li>Week 4: HTML5/JS Visuals (Toggle on click dot->check->box)</li>
+	<li>Week 5: HTML5/JS Delete button</li>
+	<li>Week 6: Testing</li>
+	<li>Week 7: UI Design & Dev</li>
+	<li>Week 8:</li>
+	<li>Week 9:</li>
+	<li>Week 10:</li>
 	<li>Week 11: Final Tweaks</li>
 	<li>Week 12: Launch</li>
 </ol>
@@ -125,26 +134,25 @@
 <h2>Minimum Viable ToDo List Features:</h2>
 <p>
 	<ul>
-		<li>List item display (HTML/CSS/PHP,JS?)</li>
-		<li>List item input (JS)</li>
-		<li>List item add (JS)</li>
-		<li>List item delete (JS)</li>
-		<li>List save (PHP/MySQL)</li>
-		<li>List load (PHP/MySQL)</li>
+		<li>List item display (HTML5/JS/CSS)</li>
+		<li>List item input (HTML5)</li>
+		<li>List item add (HTML5)</li>
+		<li>List item delete (HTML5/JS)</li>
 	</ul>
 	<ul>
-		<li>Toggle list checkbox (CSS/JS)</li>
-		<li>Toggle list dot (CSS/JS)</li>
-		<li>List item edit (JS)</li>
+		<li>Toggle list checkbox (HTML5/CSS/JS)</li>
+		<li>Toggle list dot (HTML5/CSS/JS)</li>
+		<li>List item edit (HTML5)</li>
 	</ul>
 </p>
 <h2>Daily and Weekly Project Checkins</h2>
-<p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish</em></p>
+<p><em>Goal vs Happened, Notes, Tomorrow/Next Week's Goal, GitHub Publish, Twitter post</em></p>
 <ol>
 	<li>Week 1<ol>
 		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-1/">Day 1</a></li>
 		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-2/">Day 2</a></li>
 		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-3/">Day 3</a></li>
+		<li><a href="http://emdashbuck.com/mvp-todo-daily-report-4/">Day 4</a></li>
 		</ol>
 	</li>
 </ol>
@@ -176,9 +184,16 @@
 	</li>
 	<li>Final edit of last week's Weekly Report. (Need time to pass and come back with fresh eyes.)</li>
 	<li>Final read-through and publish. (Separate from final edit in order to catch any new errors etc that may have been introduced during edit.)</li>
-	<li>Announce on social media. (If I don't tell people I wrote something, no one will see it.)</li>
+	<li>Social Media Maintenance (accountability, maintain online presence)<ol>
+		<li>Twitter: Announce post</li>
+		<li>Facebook: Casual excited post</li>
+		<li>Pro Tumblr: Announce post, refill queue (goal > 250)</li>
+		<li>Reply to all pro messages (email, twitter, facebook, pro tumblr)</li>
+		</ol></li>
 	<li>Rewarding activity/item, e.g. dance party, danish, etc. (Important for habit reinforcement! Do not skip!)</li>
 </ol>
+<h2>Other People's Todo Lists</h2>
+https://www.blanclist.com/app/ (Mark Forster FV/P compliant)
 </div>
 </body>
 </html>
